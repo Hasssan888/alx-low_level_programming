a7805b903a0f8c34d1466748942c53af99652d73
Hasssan888 <bakrimhassan944@gmail.com>
2023-06-20 18:54:42 +0200

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: prints _putchar using putchar prototype
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char message[] = "_putchar";
+	int a;
+
+	for (a = 0; a < 8; a++)
+		_putchar (message[a]);
+	return (0);
+}
