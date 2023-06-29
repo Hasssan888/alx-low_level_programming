01e632240c0128749129560465f4ee73400fda8a
Hasssan888 <bakrimhassan944@gmail.com>
2023-06-30 00:06:52 +0200

    0x06. C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -9,25 +9,28 @@
 char *cap_string(char *str)
 {
 	int index = 0;
-	
+
 	while (str[index])
 	{
 		while (!(str[index] >= 'a' && str[index] <= 'z'))
-			ndex++;
+			index++;
 		if (str[index - 1] == ' ' ||
-				str[index - 1] == '\t' ||str[index - 1] == '\n' ||str[index - 1] == ',' ||																		    		    str[index - 1] == ';' |																	    		    str[index - 1] == '.' ||
-																						    		    str[index - 1] == '!' ||
-																								    		    str[index - 1] == '?' ||
-																										    		    str[index - 1] == '"' ||
-																												    		    str[index - 1] == '(' ||
-																														    		    str[index - 1] == ')' ||
-																																    		    str[index - 1] == '{' ||
-																																		    		    str[index - 1] == '}' ||
-																																				    		    index == 0)
-														str[index] -= 32;
-										
-												index++;
-													}
-			
-				return (str);
-				:
+				str[index - 1] == '\t' ||
+				str[index - 1] == '\n' ||
+				str[index - 1] == ',' ||
+				str[index - 1] == ';' ||
+				str[index - 1] == '.' ||
+				str[index - 1] == '!' ||
+				str[index - 1] == '?' ||
+				str[index - 1] == '"' ||
+				str[index - 1] == '(' ||
+				str[index - 1] == ')' ||
+				str[index - 1] == '{' ||
+				str[index - 1] == '}' ||
+				index == 0)
+			str[index] -= 32;
+		index++;
+	}
+
+	return (str);
+}
