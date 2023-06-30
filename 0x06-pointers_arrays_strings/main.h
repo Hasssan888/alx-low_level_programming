8b043180138e5a38f42cc1c65add41401a787a96
Hasssan888 <bakrimhassan944@gmail.com>
2023-06-30 14:34:18 +0200

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,20 +1,22 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include <stdio.h>
+int main(void)
+{
+       	int n;
+	int a[5];
+	int *p;
 
-
-
-int _putchar(char c);
-char *_strcat(char *dest, char *src);
-char *_strncat(char *dest, char *src, int n);
-char *_strncpy(char *dest, char *src, int n);
-int _strcmp(char *s1, char *s2);
-void reverse_array(int *a, int n);
-char *string_toupper(char *a);
-char *cap_string(char *a);
-char *leet(char *a);
-char *rot13(char *a);
-void print_number(int n);
-char *infinite_add(char *n1, char *n2, char *r, int size_r);
-void print_buffer(char *b, int size);
-
-#endif
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
